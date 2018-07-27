469a51e7b22fb435d08b24b18f40618780a3abf1
Anton V <anton.v6@mail.ru>
2018-07-27 16:42:19 +0400

    Initial commit

--- Server/ServerForm.cs
+++ Server/ServerForm.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Server
